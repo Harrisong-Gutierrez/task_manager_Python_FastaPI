07c754bbe2c9e33f39fc5b6e4ba54f5b9e4454e6
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-09-10 15:10:54 -0600

    changing on event

--- app/main.py
+++ app/main.py
@@ -2,24 +2,33 @@ from fastapi import FastAPI
 from sqlalchemy import text
 from app.api.endpoints import tasks, auth, health, users
 from app.infra.database import db
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Task Manager API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    print("Iniciando aplicación Task Manager API...")
+
+    try:
+        with db.engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+            print("✅ Conexión exitosa a la base de datos PostgreSQL")
+    except Exception as e:
+        print(f"❌ Error de conexión a la base de datos: {e}")
+
+    yield
+
+    print("🔻 Cerrando aplicación...")
+
+
+app = FastAPI(title="Task Manager API", lifespan=lifespan)
+
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
 app.include_router(health.router)
 app.include_router(users.router)
-
-
-@app.on_event("startup")
-async def startup_event():
-
-    try:
-        with db.engine.connect() as connection:
-            result = connection.execute(text("SELECT 1"))
-            print("Conexión exitosa a la base de datos PostgreSQL")
-    except Exception as e:
-        print(f"Error de conexión a la base de datos: {e}")
 
 
 @app.get("/")
