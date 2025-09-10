097a30a6ded627754c35e52573a7d45a08ad4ad1
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-09-10 13:19:14 -0600

    good request

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,32 +1,31 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+from supabase_auth import User
+from app.services.user_service import UserService
+from app.core.dependencies import get_user_service  # Importar la nueva dependencia
 from app.core.config import settings
-from app.infra.repositories.user_repository import UserRepository
-from app.services.auth_service import AuthService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    user_service: UserService = Depends(get_user_service)  # Inyectar el servicio
+) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
+        
+        user = user_service.get_user_by_email(email)
+        if user is None:
+            raise credentials_exception
+        return user
     except JWTError:
         raise credentials_exception
-
-    user_repository = UserRepository()
-    auth_service = AuthService(user_repository)
-    user = auth_service.get_user_by_email(email)
-    if user is None:
-        raise credentials_exception
-    return user
